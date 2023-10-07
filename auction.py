0da72f190e58ef33db09c1326e280fd1c6514cdb
Pedro Argento <pedro.argento@gmail.com>
2023-10-06 22:27:11 -0300

    feat: add base auction engine

---
+++ auction.py
@@ -0,0 +1,48 @@
+from dataclasses import dataclass
+from collections.abc import Iterable
+from itertools import accumulate, starmap
+from functools import reduce
+
+@dataclass
+class Auction:
+    id: str
+    end_time: int
+    lock_time: int
+    volume_limit: float
+    reserve_price: float
+
+@dataclass
+class Bid:
+    auction_id: str
+    timestamp: int
+    volume: float
+    price: float
+    bidder: str
+
+@dataclass
+class BidOutput:
+    bidder: str
+    amount_sent: float
+    amount_fullfiled: float
+
+@dataclass
+class AuctionOutput:
+    bid_outputs: Iterable[BidOutput]
+    sorted_bids: Iterable[Bid]
+
+def filter_bids(bids: Iterable[Bid], auction_id: str, timestamp_upper_limit: int, minimum_bid_price: float) -> Iterable[Bid]:
+    return filter(lambda bid: bid.auction_id == auction_id and bid.timestamp <= timestamp_upper_limit and bid.price >= minimum_bid_price, bids)
+
+def auction_output(bids: Iterable[Bid], volume_limit: float) -> AuctionOutput:
+    sorted_bids = sorted(bids, key = lambda bid: bid.price, reverse = True)
+    accumulated_budget = accumulate(sorted_bids, lambda acc, bid: acc-bid.volume, initial=volume_limit) #each bid consumes the auction amount limit.
+    def fullfiled_volume(bid, budget):
+        return BidOutput(bid.bidder, bid.volume, max(min(budget,bid.volume), 0))
+    outputs = starmap(fullfiled_volume, zip(sorted_bids, accumulated_budget))
+    return AuctionOutput(outputs, sorted_bids)
+
+def auction_price(output: AuctionOutput) -> float:
+    sorted_bids = output.sorted_bids
+    outputs = output.bid_outputs
+    bid, _ = min(filter(lambda x: x[1].amount_fullfiled > 0, zip(sorted_bids, outputs)), key=lambda x: x[0].price)
+    return bid.price
---
+++ test.py
@@ -0,0 +1,40 @@
+import unittest
+from auction import *
+
+class BidsTest(unittest.TestCase):
+    def setUp(self):
+        self.auction = Auction("a", 200, 100, 100, 0.5)
+        self.b1 = Bid("a", 95, 100, 0.6, "aaaa")
+        self.b2 = Bid("a", 95, 100, 0.4, "aaaa")
+        self.b3 = Bid("a", 1100, 110, 0.7, "aaaa")
+        self.b4 = Bid("b", 95, 100, 0.71, "aaaa")
+        self.b5 = Bid("a", 95, 90, 0.8, "abaa")
+
+        self.bid_list = [self.b1, self.b2, self.b3, self.b4, self.b5]
+
+    def test_valid_bids(self):
+        expected_filtered_bids = [self.b1, self.b5]
+        filtered_bids = list(filter_bids([self.b1, self.b2,self.b3,self.b4,self.b5], self.auction.id, self.auction.end_time, self.auction.reserve_price))
+        self.assertEqual(filtered_bids, expected_filtered_bids, 'wrong list of filtered bids')
+
+    def test_auction_output(self):
+        VOLUME_LIMIT = 250
+        bid_output1 = BidOutput(self.b1.bidder, self.b1.volume, 0)
+        bid_output2 = BidOutput(self.b2.bidder, self.b2.volume, 0)
+        bid_output3 = BidOutput(self.b3.bidder, self.b3.volume, 60)
+        bid_output4 = BidOutput(self.b4.bidder, self.b4.volume, 100)
+        bid_output5 = BidOutput(self.b5.bidder, self.b5.volume, 90)
+        expected_outputs = [bid_output5, bid_output4, bid_output3, bid_output1, bid_output2]
+        bids_outputs = list(auction_output(self.bid_list, VOLUME_LIMIT).bid_outputs)
+        self.assertEqual(bids_outputs, expected_outputs, 'wrong list of outputs bids')
+        sum_of_fullfill = sum([output.amount_fullfiled for output in bids_outputs])
+        self.assertEqual(sum_of_fullfill, VOLUME_LIMIT)
+
+    def test_auction_price(self):
+        VOLUME_LIMIT = 250
+        auction_outputs =  auction_output(self.bid_list, VOLUME_LIMIT)
+        price = auction_price(auction_outputs)
+        self.assertEqual(price, 0.7)
+
+if __name__ == '__main__':
+    unittest.main()
