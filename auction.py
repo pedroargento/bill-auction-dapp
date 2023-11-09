d25968c773478a9d19aecbbab2872e13f2671d28
Pedro Argento <pedro.argento@gmail.com>
2023-11-09 18:26:26 -0300

    style: fix code formating

--- auction.py
+++ auction.py
@@ -11,12 +11,14 @@ class Mine(NamedTuple):
     owner: Address
     erc20_address: Address
 
+
 class Auction(NamedTuple):
     id: str
     end_time: int
     lock_time: int
     volume_limit: float
     reserve_price: float
+
 
 class Bid(NamedTuple):
     auction_id: str
@@ -25,16 +27,21 @@ class Bid(NamedTuple):
     price: float
     bidder: Address
 
+
 class BidOutput(NamedTuple):
     bidder: Address
     amount_sent: float
     amount_fullfiled: float
 
+
 class AuctionOutput(NamedTuple):
     bid_outputs: Iterable[BidOutput]
     sorted_bids: Iterable[Bid]
 
-FunctionCall = Enum('FunctionCall', ['TRANSFER', 'MINT'])
+
+FunctionCall = Enum("FunctionCall", ["TRANSFER", "MINT"])
+
+
 class Voucher(NamedTuple):
     target_contract: Address
     function_call: FunctionCall
@@ -42,39 +49,102 @@ class Voucher(NamedTuple):
     amount: float
     timestamp_locked: bool
 
-def filter_bids(bids: Iterable[Bid], auction_id: str, timestamp_upper_limit: int, minimum_bid_price: float) -> Iterable[Bid]:
-    return filter(lambda bid: bid.auction_id == auction_id and bid.timestamp <= timestamp_upper_limit and bid.price >= minimum_bid_price, bids)
+
+def filter_bids(
+    bids: Iterable[Bid],
+    auction_id: str,
+    timestamp_upper_limit: int,
+    minimum_bid_price: float,
+) -> Iterable[Bid]:
+    return filter(
+        lambda bid: bid.auction_id == auction_id
+        and bid.timestamp <= timestamp_upper_limit
+        and bid.price >= minimum_bid_price,
+        bids,
+    )
+
 
 def auction_output(bids: Iterable[Bid], volume_limit: float) -> AuctionOutput:
-    sorted_bids = sorted(bids, key = lambda bid: bid.price, reverse = True)
-    accumulated_budget = accumulate(sorted_bids, lambda acc, bid: acc-bid.volume, initial=volume_limit) #each bid consumes the auction amount limit.
+    sorted_bids = sorted(bids, key=lambda bid: bid.price, reverse=True)
+    accumulated_budget = accumulate(
+        sorted_bids, lambda acc, bid: acc - bid.volume, initial=volume_limit
+    )  # each bid consumes the auction amount limit.
+
     def fullfiled_volume(bid, budget):
-        return BidOutput(bid.bidder, bid.volume, max(min(budget,bid.volume), 0))
+        return BidOutput(bid.bidder, bid.volume, max(min(budget, bid.volume), 0))
+
     outputs = starmap(fullfiled_volume, zip(sorted_bids, accumulated_budget))
     return AuctionOutput(outputs, sorted_bids)
+
 
 def auction_price(output: AuctionOutput) -> float:
     sorted_bids = output.sorted_bids
     outputs = output.bid_outputs
-    bid, _ = min(filter(lambda x: x[1].amount_fullfiled > 0, zip(sorted_bids, outputs)), key=lambda x: x[0].price)
+    bid, _ = min(
+        filter(lambda x: x[1].amount_fullfiled > 0, zip(sorted_bids, outputs)),
+        key=lambda x: x[0].price,
+    )
     return bid.price
+
 
 def generate_bid_vouchers(output: BidOutput, price: float) -> Iterable[Voucher]:
     not_fullfiled = output.amount_sent - output.amount_fullfiled
-    mint_amount = max((1 - price)*output.amount_fullfiled//price, 0)
-    burn_amount = max((price - 1)*output.amount_fullfiled//price, 0)
-    return_voucher = Voucher(Address("token_contract"), FunctionCall.TRANSFER, output.bidder, not_fullfiled, timestamp_locked = False)
-    bid_portion_voucher = Voucher(Address("token_contract"), FunctionCall.TRANSFER, to=output.bidder, amount=output.amount_fullfiled - burn_amount, timestamp_locked = True)
-    mint_voucher = Voucher(Address("mine_contract"), FunctionCall.MINT, to=output.bidder, amount=mint_amount, timestamp_locked = True)
-    burn_voucher = Voucher(Address("token_contract"), FunctionCall.TRANSFER, to=Address('mine_contract'), amount=burn_amount, timestamp_locked = True)
-    return filter(lambda voucher: voucher.amount > 0, [return_voucher, bid_portion_voucher, mint_voucher, burn_voucher])
+    mint_amount = max((1 - price) * output.amount_fullfiled // price, 0)
+    burn_amount = max((price - 1) * output.amount_fullfiled // price, 0)
+    return_voucher = Voucher(
+        Address("token_contract"),
+        FunctionCall.TRANSFER,
+        output.bidder,
+        not_fullfiled,
+        timestamp_locked=False,
+    )
+    bid_portion_voucher = Voucher(
+        Address("token_contract"),
+        FunctionCall.TRANSFER,
+        to=output.bidder,
+        amount=output.amount_fullfiled - burn_amount,
+        timestamp_locked=True,
+    )
+    mint_voucher = Voucher(
+        Address("mine_contract"),
+        FunctionCall.MINT,
+        to=output.bidder,
+        amount=mint_amount,
+        timestamp_locked=True,
+    )
+    burn_voucher = Voucher(
+        Address("token_contract"),
+        FunctionCall.TRANSFER,
+        to=Address("mine_contract"),
+        amount=burn_amount,
+        timestamp_locked=True,
+    )
+    return filter(
+        lambda voucher: voucher.amount > 0,
+        [return_voucher, bid_portion_voucher, mint_voucher, burn_voucher],
+    )
+
 
 def auction_vouchers(outputs: Iterable[BidOutput], price: float) -> Iterable[Voucher]:
     return chain(map(lambda output: generate_bid_vouchers(output, price), outputs))
 
-def aggregate_vouchers(vouchers: Iterable[Voucher]) -> Iterable[Voucher]:
-    voucher_key=lambda voucher: (voucher.target_contract, voucher.function_call, voucher.to, voucher.timestamp_locked)
-    sorted_vouchers = sorted(vouchers, key = voucher_key)
-    grouped_vouchers = groupby(sorted_vouchers, key = voucher_key)
-    return [Voucher(target_contract = key[0], function_call = key[1], to = key[2], timestamp_locked = key[3], amount = sum((voucher.amount for voucher in group))) for key, group in grouped_vouchers]
 
+def aggregate_vouchers(vouchers: Iterable[Voucher]) -> Iterable[Voucher]:
+    voucher_key = lambda voucher: (
+        voucher.target_contract,
+        voucher.function_call,
+        voucher.to,
+        voucher.timestamp_locked,
+    )
+    sorted_vouchers = sorted(vouchers, key=voucher_key)
+    grouped_vouchers = groupby(sorted_vouchers, key=voucher_key)
+    return [
+        Voucher(
+            target_contract=key[0],
+            function_call=key[1],
+            to=key[2],
+            timestamp_locked=key[3],
+            amount=sum((voucher.amount for voucher in group)),
+        )
+        for key, group in grouped_vouchers
+    ]
--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import unittest
 from auction import *
 
+
 class BidsTest(unittest.TestCase):
     def setUp(self):
         self.auction = Auction("auction_a", 200, 100, 100, 0.5)
@@ -14,8 +15,17 @@ class BidsTest(unittest.TestCase):
 
     def test_valid_bids(self):
         expected_filtered_bids = [self.b1, self.b5]
-        filtered_bids = list(filter_bids([self.b1, self.b2,self.b3,self.b4,self.b5], self.auction.id, self.auction.end_time, self.auction.reserve_price))
-        self.assertEqual(filtered_bids, expected_filtered_bids, 'wrong list of filtered bids')
+        filtered_bids = list(
+            filter_bids(
+                [self.b1, self.b2, self.b3, self.b4, self.b5],
+                self.auction.id,
+                self.auction.end_time,
+                self.auction.reserve_price,
+            )
+        )
+        self.assertEqual(
+            filtered_bids, expected_filtered_bids, "wrong list of filtered bids"
+        )
 
     def test_auction_output(self):
         VOLUME_LIMIT = 250
@@ -24,42 +34,149 @@ class BidsTest(unittest.TestCase):
         bid_output3 = BidOutput(self.b3.bidder, self.b3.volume, 60)
         bid_output4 = BidOutput(self.b4.bidder, self.b4.volume, 100)
         bid_output5 = BidOutput(self.b5.bidder, self.b5.volume, 90)
-        expected_outputs = [bid_output5, bid_output4, bid_output3, bid_output1, bid_output2]
+        expected_outputs = [
+            bid_output5,
+            bid_output4,
+            bid_output3,
+            bid_output1,
+            bid_output2,
+        ]
         bids_outputs = list(auction_output(self.bid_list, VOLUME_LIMIT).bid_outputs)
-        self.assertEqual(bids_outputs, expected_outputs, 'wrong list of outputs bids')
+        self.assertEqual(bids_outputs, expected_outputs, "wrong list of outputs bids")
         sum_of_fullfill = sum([output.amount_fullfiled for output in bids_outputs])
         self.assertEqual(sum_of_fullfill, VOLUME_LIMIT)
 
     def test_auction_price(self):
         VOLUME_LIMIT = 250
-        auction_outputs =  auction_output(self.bid_list, VOLUME_LIMIT)
+        auction_outputs = auction_output(self.bid_list, VOLUME_LIMIT)
         price = auction_price(auction_outputs)
         self.assertEqual(price, 0.7)
 
     def test_generate_bid_vouchers_no_fullfiled(self):
         price = 0.7
-        output = BidOutput(Address('alice'), 100, 0)
-        expected = [Voucher(Address('token_contract'), FunctionCall.TRANSFER, Address('alice'), 100, False)]
+        output = BidOutput(Address("alice"), 100, 0)
+        expected = [
+            Voucher(
+                Address("token_contract"),
+                FunctionCall.TRANSFER,
+                Address("alice"),
+                100,
+                False,
+            )
+        ]
         self.assertEqual(list(generate_bid_vouchers(output, price)), expected)
 
     def test_generate_bid_vouchers_mint(self):
         price = 0.7
-        output = BidOutput(Address('alice'), 100, 70)
-        expected = [Voucher(Address('token_contract'), FunctionCall.TRANSFER, Address('alice'), 30, False), Voucher(Address('token_contract'), FunctionCall.TRANSFER, Address('alice'), 70, True), Voucher(Address('mine_contract'), FunctionCall.MINT, Address('alice'), 30, True)]
+        output = BidOutput(Address("alice"), 100, 70)
+        expected = [
+            Voucher(
+                Address("token_contract"),
+                FunctionCall.TRANSFER,
+                Address("alice"),
+                30,
+                False,
+            ),
+            Voucher(
+                Address("token_contract"),
+                FunctionCall.TRANSFER,
+                Address("alice"),
+                70,
+                True,
+            ),
+            Voucher(
+                Address("mine_contract"), FunctionCall.MINT, Address("alice"), 30, True
+            ),
+        ]
         self.assertEqual(list(generate_bid_vouchers(output, price)), expected)
 
     def test_generate_bid_vouchers_burn(self):
         price = 1.1
-        output = BidOutput(Address('alice'), 100, 80)
-        expected = [Voucher(Address('token_contract'), FunctionCall.TRANSFER, Address('alice'), 20, False), Voucher(Address('token_contract'), FunctionCall.TRANSFER, Address('alice'), 73, True), Voucher(Address('token_contract'), FunctionCall.TRANSFER, Address('mine_contract'), int(7), True)]
+        output = BidOutput(Address("alice"), 100, 80)
+        expected = [
+            Voucher(
+                Address("token_contract"),
+                FunctionCall.TRANSFER,
+                Address("alice"),
+                20,
+                False,
+            ),
+            Voucher(
+                Address("token_contract"),
+                FunctionCall.TRANSFER,
+                Address("alice"),
+                73,
+                True,
+            ),
+            Voucher(
+                Address("token_contract"),
+                FunctionCall.TRANSFER,
+                Address("mine_contract"),
+                int(7),
+                True,
+            ),
+        ]
         self.assertEqual(list(generate_bid_vouchers(output, price)), expected)
 
     def test_aggregate_vouchers(self):
-        vouchers = [Voucher(Address('token_contract'), FunctionCall.TRANSFER, Address('mine_contract'), int(57), True), Voucher(Address('token_contract'), FunctionCall.TRANSFER, Address('alice'), 20, False), Voucher(Address('token_contract'), FunctionCall.TRANSFER, Address('alice'), 73, True), Voucher(Address('token_contract'), FunctionCall.TRANSFER, Address('mine_contract'), int(7), True)]
+        vouchers = [
+            Voucher(
+                Address("token_contract"),
+                FunctionCall.TRANSFER,
+                Address("mine_contract"),
+                int(57),
+                True,
+            ),
+            Voucher(
+                Address("token_contract"),
+                FunctionCall.TRANSFER,
+                Address("alice"),
+                20,
+                False,
+            ),
+            Voucher(
+                Address("token_contract"),
+                FunctionCall.TRANSFER,
+                Address("alice"),
+                73,
+                True,
+            ),
+            Voucher(
+                Address("token_contract"),
+                FunctionCall.TRANSFER,
+                Address("mine_contract"),
+                int(7),
+                True,
+            ),
+        ]
         aggregated_vouchers = set(aggregate_vouchers(vouchers))
-        expected = set([Voucher(Address('token_contract'), FunctionCall.TRANSFER, Address('mine_contract'), int(64), True), Voucher(Address('token_contract'), FunctionCall.TRANSFER, Address('alice'), 20, False), Voucher(Address('token_contract'), FunctionCall.TRANSFER, Address('alice'), 73, True)])
+        expected = set(
+            [
+                Voucher(
+                    Address("token_contract"),
+                    FunctionCall.TRANSFER,
+                    Address("mine_contract"),
+                    int(64),
+                    True,
+                ),
+                Voucher(
+                    Address("token_contract"),
+                    FunctionCall.TRANSFER,
+                    Address("alice"),
+                    20,
+                    False,
+                ),
+                Voucher(
+                    Address("token_contract"),
+                    FunctionCall.TRANSFER,
+                    Address("alice"),
+                    73,
+                    True,
+                ),
+            ]
+        )
         self.assertEqual(aggregated_vouchers, expected)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
